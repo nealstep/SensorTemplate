ff2c8638a501f317d0ff4934595a050c86e5940b
Neal Stephenson <neal@bakerst.org>
2021-01-17 09:57:58 -0500

    finishes fix.sh

--- fix.sh
+++ fix.sh
@@ -12,9 +12,15 @@ nameL=${name,,};
 echo $nameU
 echo $nameL
 
-sed -e "s/SENSOR_/${nameU}_/g" -e "s/sensor_/${nameU}_/g" < include/sensor.h
-sed -e "s/sensor_/${nameL}_/g" -e "s/sensor\./${nameL}./g" < src/sensor.cpp
-sed -e "s/sensor_/${nameL}_/g" -e "s/sensor\./${nameL}./g" < src/main.cpp
-
+sed -e "s/SENSOR_/${nameU}_/g" -e "s/sensor_/${nameU}_/g" \
+    < include/sensor.h > include/${nameL}.h
+sed -e "s/sensor_/${nameL}_/g" -e "s/sensor\./${nameL}./g" \
+    < src/sensor.cpp > src/${nameL}.cpp
+sed -e "s/sensor_/${nameL}_/g" -e "s/sensor\./${nameL}./g" \
+    < src/main.cpp > src/main_n.cpp
+mv include/sensor.h include/sensor.h.old
+mv include/sensor.cpp include/sensor.cpp.old
+mv include/main.cpp include/main.cpp.old
+mv include/main_n.cpp include/main.cpp
 
 
